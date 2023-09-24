dc265be0447c5347b2209872ed2248f37aa0a587
Ashu <ashukr2218@gmail.com>
2023-09-24 13:09:00 +0530

    added test js code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-
+import  openai
+openai.api_key = "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"
 
 
 
@@ -17,9 +18,12 @@ def chat():
     return get_Chat_response(input)
 
 
-# def get_Chat_response(text):
+def get_Chat_response(text):
+    response = openai.Completion.create(engine="gpt-3.5-turbo-instruct" , prompt=input , max_tokens=50)
+    return response["choices"][0]["text"]
+
 
     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/serve.js
@@ -0,0 +1,27 @@
+const chatbox = jQuery.noConflict();
+
+chatbox(() => {
+  chatbox(".chatbox-open").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeIn()
+  );
+
+  chatbox(".chatbox-close").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeOut()
+  );
+
+  chatbox(".chatbox-maximize").click(() => {
+    chatbox(".chatbox-popup, .chatbox-open, .chatbox-close").fadeOut();
+    chatbox(".chatbox-panel").fadeIn();
+    chatbox(".chatbox-panel").css({ display: "flex" });
+  });
+
+  chatbox(".chatbox-minimize").click(() => {
+    chatbox(".chatbox-panel").fadeOut();
+    chatbox(".chatbox-popup, .chatbox-open, .chatbox-close").fadeIn();
+  });
+
+  chatbox(".chatbox-panel-close").click(() => {
+    chatbox(".chatbox-panel").fadeOut();
+    chatbox(".chatbox-open").fadeIn();
+  });
+});
