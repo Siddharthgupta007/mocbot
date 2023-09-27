e39fcc469bd3e1061577cdc36686d29d846cecfb
Ashu <ashukr2218@gmail.com>
2023-09-27 10:41:26 +0530

    integrated langchain in bot.

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template,request, jsonify
 import  openai
 from logging import FileHandler,WARNING
-openai.api_key = ""
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
-
+chat = ChatOpenAI(openai_api_key="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")
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -13,14 +18,19 @@ def index():
 
 
 @app.route("/get", methods=["GET", "POST"])
-def chat():
-    message =request.form['msg'] 
-    input = message
+def bot():
+    userInput =request.form['msg'] 
+    input = userInput
     return get_Chat_response(input)
 
 def get_Chat_response(text):
-     response = openai.Completion.create(engine="gpt-3.5-turbo-instruct" , prompt=text , max_tokens=100)
-     return response["choices"][0]["text"]
+    #  response = openai.Completion.create(engine="gpt-3.5-turbo-instruct" , prompt=text , max_tokens=100)
+    #  return response["choices"][0]["text"]
+    message = [
+    SystemMessage(content="You are a helpful assistant that answers questions related to the mining and coal industry of india and acts related to coals and mining in india ONLY and if question related to any other topic is asked you simply respond with 'I can only answer questions related to Ministry of Coals and Mining of India and its policies'.generate the answer in bullet points.limit your responses to under 100 words "),
+    HumanMessage(content=text)]
+    output = chat(messages=message)
+    return output.content
    
     
     
