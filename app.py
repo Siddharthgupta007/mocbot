b8bfbc2bc3217c8018848e041110e1d33beac0c8
Siddharth <siddharth11tha@gmail.com>
2023-09-27 12:59:58 +0530

    added langchain to api for precised output

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.schema import (
     SystemMessage
 )
 
-chat = ChatOpenAI(openai_api_key="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")
+chat = ChatOpenAI(openai_api_key="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")
 
 app = Flask(__name__, template_folder = 'templates')
 
@@ -27,7 +27,7 @@ def get_Chat_response(text):
     #  response = openai.Completion.create(engine="gpt-3.5-turbo-instruct" , prompt=text , max_tokens=100)
     #  return response["choices"][0]["text"]
     message = [
-    SystemMessage(content="You are a helpful assistant that answers questions related to the mining and coal industry of india and acts related to coals and mining in india ONLY and if question related to any other topic is asked you simply respond with 'I can only answer questions related to Ministry of Coals and Mining of India and its policies'.generate the answer in bullet points.limit your responses to under 100 words "),
+    SystemMessage(content="You are a helpful assistant that answers questions related to the mining and coal industry of india and acts related to coals and mining in india ONLY and if question related to any other topic is asked you simply respond with 'I can only answer questions related to Ministry of Coals and Mining of India and its policies'.generate the answer in bullet points.limit your responses to under 25 words "),
     HumanMessage(content=text)]
     output = chat(messages=message)
     return output.content
