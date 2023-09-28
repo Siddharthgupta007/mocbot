117dcf2ead40dc7268c34aebf27831e8c957c070
Siddharth <siddharth11tha@gmail.com>
2023-09-28 21:41:09 +0530

    added new UI for chatbot

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_Chat_response(text):
     #  response = openai.Completion.create(engine="gpt-3.5-turbo-instruct" , prompt=text , max_tokens=100)
     #  return response["choices"][0]["text"]
     message = [
-    SystemMessage(content="You are a helpful assistant that answers questions related to the mining and coal industry of india and acts related to coals and mining in india ONLY and if question related to any other topic is asked you simply respond with 'I can only answer questions related to Ministry of Coals and Mining of India and its policies'.generate the answer in bullet points.limit your responses to under 25 words "),
+    SystemMessage(content="You are a helpful assistant that answers questions related to the mining and coal industry of india and acts related to coals and mining in india ONLY and if question related to any other topic is asked you simply respond with 'I can only answer questions related to Ministry of Coals and Mining of India and its policies'.generate the answer in bullet points.limit your responses to under 10 words "),
     HumanMessage(content=text)]
     output = chat(messages=message)
     return output.content
